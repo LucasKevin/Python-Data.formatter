c96a06fcd2e662ae66c43bf0fcc73113508efd20
Lucas Kevin <84647287+LucasKevin@users.noreply.github.com>
2022-11-19 11:34:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+while True:
+ format = input('O que você quer formatar?\n '
+ '1 - CPF\n '
+ '2 - CNPJ\n'
+ '3 - MEC \n '
+ '>>')
+
+ if format == '1':
+     s = input("CPF: ")
+     test = ''.join(filter(lambda i: i if i.isdigit() else None, s))
+     cpf = test[:3] + "." + test[3:6] + "." + test[6:9] + "-" + test[9:]
+     print(cpf)
+     break
+ elif format == '2':
+     s = input("CNPJ: ")
+     test = ''.join(filter(lambda i: i if i.isdigit() else None, s))
+     cnpj = test[:2] + "." + test[2:5] + "." + test[5:8] + "/" + test[8:12] + "-" + test[12:14]
+     print(cnpj)
+     break
+ elif format == '3':
+     s = input('Digite o MEC: ')
+     test = ''.join(filter(lambda i: i if i.isdigit() else None, s))
+     cnpj = test[:4] + "-" + test[4:8] + "-" + test[8:12]
+     print(cnpj)
+     break
+ else:
+     print('Opção Inválida!')
+     continue
+
+
+
+
+
+
+
+
+
+
